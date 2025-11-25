7d6c22f15855772b58d1e20fe1306acf14b89b33
Jelle Bens <jellebens@outlook.com>
2025-11-25 19:56:58 +0100

    Added ingress for argoscd

--- setup-argosCD.sh
+++ setup-argosCD.sh
@@ -1,20 +1,29 @@
+#!/bin/bash
+
+namespace=argocd-system
+
+
 minikube delete
 minikube start --driver=docker
  
-kubectl create ns argocd
 
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/v2.5.8/manifests/install.yaml
 
-kubectl get all -n argocd
+# kubectl create ns $namespace
 
+# kubectl apply -n $namespace -f https://raw.githubusercontent.com/argoproj/argo-cd/v2.5.8/manifests/install.yaml
+
+helm repo add argo https://argoproj.github.io/argo-helm
+helm repo update
+
+helm install argocd argo/argo-cd --namespace $namespace --create-namespace --version 9.1.4 -f argocd-values.yaml
 
 echo "Waiting for argocd-server pod to be ready..."
-kubectl wait --namespace argocd --for=condition=Ready pod -l app.kubernetes.io/name=argocd-server --timeout=300s
+kubectl wait --namespace $namespace --for=condition=Ready pod -l app.kubernetes.io/name=argocd-server --timeout=300s 
 echo "argocd-server is ready!"
 
-kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
+kubectl get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" -n $namespace | base64 -d; echo
 
-helm upgrade --install cluster-bootstrap bootstrap
+helm upgrade --install cluster-bootstrap bootstrap --create-namespace --namespace argocd
 
 
 
